0c96992a08cca95409339efe1d5c2d2feab8f844
arman.nasrollahi <Arman.Nasrollahi@home24.de>
2021-06-24 16:13:12 +0200

    the App1 is finished

---
+++ Udemy_Advance_Python/Gemometry_Game/main.py
@@ -0,0 +1,20 @@
+import turtle
+
+tr = turtle.Turtle()
+
+tr.penup()
+
+tr.goto(20, 30)
+
+tr.pendown()
+
+tr.forward(100)
+tr.left(90)
+tr.forward(200)
+tr.left(90)
+tr.forward(100)
+tr.left(90)
+tr.forward(200)
+
+turtle.done()
+
