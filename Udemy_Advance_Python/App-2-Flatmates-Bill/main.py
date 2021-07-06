36e26d9cfcd5492f40b61fa8f679cba3a0cfda26
arman.nasrollahi <arman.nasrollahi@home24.de>
2021-07-06 15:26:07 +0200

    pdf generator is added

--- Udemy_Advance_Python/App-2-Flatmates-Bill/main.py
+++ Udemy_Advance_Python/App-2-Flatmates-Bill/main.py
@@ -1,3 +1,9 @@
+import os.path
+import webbrowser
+
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as
@@ -21,7 +27,7 @@ class Flatmate:
 
     def pays(self, bill, flatmate2):
         weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
-        to_pay = bill.amount * weight
+        to_pay = round(bill.amount * weight, 2)
         return to_pay
 
 
@@ -36,7 +42,42 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        flatmate1_pay = str(flatmate1.pays(bill=bill, flatmate2=flatmate2))
+        flatmate2_pay = str(flatmate2.pays(bill=bill, flatmate2=flatmate1))
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+        pdf.set_font(family='Times', size=24, style='B')
+
+        pdf.image("house.jpg", w=30, h=30)
+
+        # Insert title
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        pdf.set_font(family='Times', size=14, style='B')
+
+        # Insert Period label and value
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
+
+        webbrowser.open('file://' + os.path.realpath(self.filename))
 
 
 the_bill = Bill(amount=120, period="March 2021")
@@ -45,3 +86,6 @@ merry = Flatmate(name="Merry", days_in_house=25)
 
 print("John pays:", john.pays(bill=the_bill, flatmate2=merry))
 print("Merry pays:", merry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=merry, flatmate2=john, bill=the_bill)
