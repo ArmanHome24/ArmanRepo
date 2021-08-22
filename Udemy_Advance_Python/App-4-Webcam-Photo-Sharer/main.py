6733714d8a62ab602b61458e8f0491988f2d7ae8
arman.nasrollahi <arman.nasrollahi@home24.de>
2021-08-22 19:24:59 +0200

    Image screen is implemented

--- Udemy_Advance_Python/App-4-Webcam-Photo-Sharer/main.py
+++ Udemy_Advance_Python/App-4-Webcam-Photo-Sharer/main.py
@@ -1,8 +1,11 @@
 import time
 
 from kivy.app import App
+from kivy.core.clipboard import Clipboard
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+
+import webbrowser
 
 Builder.load_file('frontend.kv')
 
@@ -22,6 +25,26 @@ class CameraScreen(Screen):
         current_time = time.strftime('%y%m%d-%H%M%S')
         file_path = f"./resources/{current_time}.png"
         self.ids.camera.export_to_png(file_path)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = file_path
+
+
+class ImageScreen(Screen):
+    """ I did not implement all functionalities
+     """
+    def __init__(self, **kwargs):
+        super(ImageScreen, self).__init__()
+        self.file_path = ''
+
+    def create_link(self):
+        self.file_path = self.ids.img.source
+        self.ids.lbl_link.text = self.file_path
+
+    def copy_link(self):
+        Clipboard.copy(self.file_path)
+
+    def open_link(self):
+        webbrowser.open(self.file_path)
 
 
 class RootWidget(ScreenManager):
